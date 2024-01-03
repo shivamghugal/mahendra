2dc93aefb3205303b1becb1b61f16ef759d6f096
Shivam <shivamghugal29@gmail.com>
2024-01-03 14:09:53 +0530

    second commit

---
+++ Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Class1
+{
+	public Class1()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+
+		Console.WriteLine("shivam");
+		Console.WriteLine("mahendra");
+	}
+}
